0a40bb7e49d3eae7f864d0f9c7758cf0027528f0
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2022-09-15 12:01:00 -0300

    CRUD - Criando diretiva estrutural for (apenas documentacao)

---
+++ src/app/directives/for.directive.ts
@@ -0,0 +1,25 @@
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+
+@Directive({
+  selector: '[myFor]',
+})
+export class ForDirective implements OnInit {
+  @Input('myForEm') numbers: number[];
+
+  constructor(
+    private _container: ViewContainerRef,
+    private _template: TemplateRef<any>
+  ) {}
+
+  ngOnInit(): void {
+    for (let number of this.numbers){
+      this._container.createEmbeddedView(this._template)
+    }
+  }
+}
